d3cf15574320aa2be51fa7d7ed074d90c0cdccd8
carson5326 <keilitz@umich.edu>
2024-03-02 14:19:57 -0500

    added basic form

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-const myLibrary = [];
+// const myLibrary = [];
 
-function Book() {
-  // the constructor...
-}
+// function Book() {
+//   // the constructor...
+// }
 
-function addBookToLibrary() {
-  // do stuff here
-}
+// function addBookToLibrary() {
+//   // do stuff here
+// }
 
-function render() {
-  // do stuff here
-}
+// function render() {
+//   // do stuff here
+// }
 
 
-const dialog = document.querySelector('dialog');
-const button = document.querySelector('resturant-adder');
-const 
+const button = document.querySelector(".resturant-button");
+const dialog = document.querySelector(".dialog");
+const jsCloseBtn = dialog.querySelector("#js-close")
+
+button.addEventListener('click', () => {
+  dialog.showModal();
+});
+
+jsCloseBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  dialog.close();
+});
 
 //I want a form that has a button that when clicked, opens a dialog box that has a form in it. The form will contain 
