fd94a71e65a6663d2c79c943a401ff029ae654ce
carson5326 <keilitz@umich.edu>
2024-03-01 23:55:31 -0500

    switching

---
+++ script.js
@@ -0,0 +1,20 @@
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+function render() {
+  // do stuff here
+}
+
+
+const dialog = document.querySelector('dialog');
+const button = document.querySelector('resturant-adder');
+const 
+
+//I want a form that has a button that when clicked, opens a dialog box that has a form in it. The form will contain 
